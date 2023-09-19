7df1c2c347530eec6e4e024fc0687fc9506a4ba2
Reshenee <Reshenee@gmail.com>
2023-09-19 08:06:18 +0200

    test

---
+++ Konsola_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; //Obsługuje zapytania
+using System.Text; // Kodowanie znaków ASCII i symboli
+using System.Threading.Tasks;
+
+namespace Konsola_1
+{
+    internal class Program // metoda główna
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C#");
+            Console.ReadKey();
+        }
+    }
+}
